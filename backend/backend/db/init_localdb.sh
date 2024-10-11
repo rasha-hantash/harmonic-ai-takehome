848d7ce764c4558420dd583e4ea47a2a1b38bb0d
Rasha Hantash <rasha.hantash01@gmail.com>
2024-10-10 22:46:17 -0400

    Revert "first commit"
    This reverts commit 1e4b5732c79b6128602fc2bbf9cbe9eb89bc8da2.

--- backend/backend/db/init_localdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-psql -v --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE harmonicjam;
-    \c harmonicjam;
-    DROP DATABASE IF EXISTS mydatabase;
-    DROP DATABASE IF EXISTS postgres;
-EOSQL
--- frontend/vite.config.ts
+++
@@ -1,7 +0,0 @@
-// vite.config.ts
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [react()],
-});
